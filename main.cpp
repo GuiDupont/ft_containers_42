43c016827113a51e1da8ceb9a4d06f366f48dc0e
gdupont <gdupont@student.42.fr>
2021-07-07 16:14:20 +0200

    wip vector

--- main.cpp
+++ main.cpp
@@ -6,22 +6,109 @@
 /*   By: gdupont <gdupont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 11:01:31 by gdupont           #+#    #+#             */
-/*   Updated: 2021/07/06 15:38:58 by gdupont          ###   ########.fr       */
+/*   Updated: 2021/07/07 14:54:39 by gdupont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.hpp"
+#include <vector>
+
+template< typename T>
+void	printVec(std::vector<T> const & v, std::string s)
+{
+	std::cout << s;
+	for (typename std::allocator<T>::size_type i = 0; i < v.size(); i++)
+		std::cout << v[i] << " ";
+	std::cout << "\n";
+}
+
+
 
 int main(void)
 {
-	int a = 2;
-	ft::vector<int> test2(3, a);
-	ft::vector<int> test(test2);
-	ft::vector<int> test3;
+	ft::vector<int> mine2(2,4);
+	ft::vector<int> mine(mine2);
+	ft::vector<int> mine3;
+	ft::vector<int> mine4;
 
-	test.printSelf();
-	test2.printSelf();
-	test3 = test;
-	test3.printSelf();
 
+	std::vector<int> them2(5, 4);
+	std::vector<int> them(them2);
+	std::vector<int> them3(5);
+	std::cout << "\n";
+
+
+	// mine.push_back(1);
+	// mine.push_back(2);
+	// mine.push_back(3);
+
+	// //std::cout << mine.capacity();
+
+	// mine.push_back(4);
+	// mine.push_back(5);
+	// mine.push_back(6);
+	// mine.push_back(7);
+	// mine.push_back(8);
+	// mine.pop_back();
+	mine2.printSelf();
+	mine3.printSelf();
+	mine3.push_back(1);
+	mine3.push_back(2);
+	mine3.push_back(3);
+	mine3.push_back(4);
+	mine3.push_back(5);
+
+
+	mine3.printSelf();
+	mine3.assign(17, 3);
+	mine3.push_back(24);
+	std::cout << "mine 3 : ";
+	mine3.printSelf();
+	std::cout << "mine 2 : ";
+	mine2.printSelf();
+	
+	mine3.swap(mine2);
+	std::cout << "mine 3 : ";
+	mine3.printSelf();
+	std::cout << "mine 2 : ";
+	mine2.printSelf();
+	mine2.resize(37);
+	std::cout << "mine 2 : ";
+	mine2.printSelf();
+	// std::cout << mine[0] << mine[1] << mine[2]
+	//  << mine[3]<< mine[4] << mine[5] << " ";
+	std::cout << them3.capacity() << " ";
+	printVec(them3, "them3 = ");
+	them3.resize(200, 3);
+	printVec(them3, "them3 = ");
+	std::cout << them3.capacity();
+	
+
+	std::cout << " \n " << mine2.max_size() << " \n ";
+	std::cout << them.max_size();
+	std::cout << mine4.empty();
+	std::cout << them.empty();
+
+
+	
+	
+	// std::cout << mine[0] << mine[1] << mine[2]
+	//  << mine[3]<< mine[4] << mine[5] << mine[6] << mine[7] << mine[8];
+	// mine.printSelf();
+	// mine.clear();
+	// mine.printSelf();
+	// std::cout << them.capacity();
+	// them.assign(10, 2);
+	// std::cout << them[0] << them[1] << them[2]
+	//   << them[3]<< them[4] << them[5] << them[6] << them[7];
+	// std::cout << them.capacity();
+
+
+	std::cout << "\n";
+
+
+	
+	// }
+
+// 
 }
--- vector.hpp
+++ vector.hpp
@@ -6,17 +6,21 @@
 /*   By: gdupont <gdupont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 08:41:22 by gdupont           #+#    #+#             */
-/*   Updated: 2021/07/06 15:35:56 by gdupont          ###   ########.fr       */
+/*   Updated: 2021/07/07 16:13:42 by gdupont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <iterator>
 
+#include <unistd.h>
+
+#define REALLOC_MULT 2
+
 namespace ft {
 
 	template <typename T, class A = std::allocator<T> >
-	class vector : public A {
+	class vector {
 	public:
 		typedef A allocator_type;
 		typedef typename A::value_type value_type; 
@@ -101,19 +105,20 @@ namespace ft {
 		// typedef std::reverse_iterator<iterator> reverse_iterator; //optional
 		// typedef std::reverse_iterator<const_iterator> const_reverse_iterator; //optional
 
-		explicit vector (const A& alloc = allocator_type()) : _alloc(alloc), _nbElem(0), 
-		_space(0) {
-			std::cout << "J'alloue un vector vide\n";
-			this->_buffer = this->_alloc.allocate(_space);
+		explicit vector (const A& alloc = allocator_type()) : _alloc(alloc), _size(0), 
+		_capacity(0) {
+			// std::cout << "J'alloue un vector vide\n";
+			this->_buffer = this->_alloc.allocate(_capacity);
 		} 
 		//default
 		
 		explicit vector (size_type n, const value_type& val = value_type(), 
-							const allocator_type& alloc = allocator_type()) : _alloc(alloc), _nbElem(n), _space(3 * n) {
-			std::cout << "J'alloue un vector de " << n <<" elem\n";
+							const allocator_type& alloc = allocator_type()) 
+							: _alloc(alloc), _size(n), _capacity(n) {
+			// std::cout << "J'alloue un vector de " << n <<" elem\n";
 			try
 			{
-				this->_buffer = this->_alloc.allocate(_space);
+				this->_buffer = this->_alloc.allocate(_capacity);
 				for (size_type i = 0; i < n; i++) {
 					this->_alloc.construct(this->_buffer + i, val);
 				}
@@ -127,13 +132,13 @@ namespace ft {
 			//  fill (2)	
 		// template <class T::iterator >
         // vector (InputIterator first, InputIterator last,
-        //         const A& alloc = allocator_type()) : _alloc(alloc), _nbElem(5)
+        //         const A& alloc = allocator_type()) : _alloc(alloc), _size(5)
 		// {
 		// 	std::cout << "J'alloue un vector de " << last - first <<" elem a base d'un range\n";
 		// 	try
 		// 	{
 		// 		this->_buffer = this->_alloc.allocate(last - first);
-		// 		for (size_type i = 0; i < this->_nbElem; i++) {
+		// 		for (size_type i = 0; i < this->_size; i++) {
 		// 			std::advance(first, 1);
 		// 			this->_alloc.construct(this->_buffer + i, first);
 		// 		}
@@ -142,41 +147,44 @@ namespace ft {
 		// 	{
 		// 		std::cout << e.what() << std::endl;
 		// 	}
-		// } //range (3)	
-		vector (const vector& x) : _alloc(x._alloc), _nbElem(x._nbElem) {
-			std::cout << "J'alloue un vector de " << _nbElem <<" elem par copy\n";
-			this->_buffer = this->_alloc.allocate(_nbElem);
-			for (size_type i = 0; i < _nbElem; i++) {
-					this->_alloc.construct(this->_buffer + i, x._buffer[i]);
+		// } //range (3)
+			
+		vector (const vector& x) : _alloc(x._alloc), _size(x._size), _capacity(x._capacity) {
+			// std::cout << "J'alloue un vector de " << _size <<" elem par copy\n";
+			this->_buffer = this->_alloc.allocate(_capacity);
+			for (size_type i = 0; i < _size; i++) {
+					_alloc.construct(this->_buffer + i, x._buffer[i]);
 				}
-		} 
-		//copy (4)	
+		}//copy (4)
+			
 		~vector() {
-			std::cout << "je vais delete un vector de " << _nbElem << " elem \n";
-			for (size_type i = 0; i < _nbElem; i++) {
+			// std::cout << "je vais delete un vector de " << _size << " elem et de capacity " << _capacity << "\n";
+			for (size_type i = 0; i < _size; i++) {
 					this->_alloc.destroy(this->_buffer + i);
 				}
-			this->_alloc.deallocate(this->_buffer, _nbElem);
+			this->_alloc.deallocate(this->_buffer, _capacity);
 		}
 
 		vector& operator=(const vector& rhs) {
-			for (size_type i = 0; i < _nbElem; i++) {
-					this->_alloc.destroy(this->_buffer + i);
-				}
-			this->_alloc.deallocate(this->_buffer, _nbElem);
-			_nbElem = rhs._nbElem;
-			this->_buffer = this->_alloc.allocate(_nbElem);
-			for (size_type i = 0; i < _nbElem; i++) {
+			reallocate(rhs._capacity);
+			_size = rhs._size;
+			for (size_type i = 0; i < _size; i++) {
 					this->_alloc.construct(this->_buffer + i, rhs._buffer[i]);
 				}
 			return (*this);
 		}
+		
 /////to delete---------------------------------------------------------------------------------------------
-		size_type getNbElem(void) const { return (this->_nbElem); } 
-		T const * getBuff(void) const{ return (this->_nbElem); } 
+		size_type getNbElem(void) const { return (this->_size); } 
+		T const * getBuff(void) const{ return (this->_size); } 
 		void printSelf(void) {
-			for (typename A::size_type i = 0; i < this->_nbElem; i++)
-				std::cout << this->_buffer[i] << " "; }
+			for (typename A::size_type i = 0; i < this->_size; i++)
+				std::cout << this->_buffer[i] << " ";
+			std::cout << "size = " << _size << " - ";
+			std::cout << " capapcity = " << _capacity << "\n"; 
+			std::cout << "\n"; 
+			
+				}
 /////to delete-------------------------------------------------------------------------------------
 					
 						
@@ -194,9 +202,28 @@ namespace ft {
 		// const_reference front() const;
 		// reference back();
 		// const_reference back() const;
-		void push_back(const T&);
-		// // void push_back (const value_type& val);
-		// void pop_back();
+		void push_back(const T& value) {
+			
+			if (!_capacity)
+			{
+				_alloc.deallocate(this->_buffer, _capacity);
+				_buffer = _alloc.allocate(1);
+				_capacity++;
+			}
+			if (_capacity == _size)
+				reallocateNCopy(_capacity * REALLOC_MULT);
+			_alloc.construct(this->_buffer + _size, value);
+			_size++;
+				
+		}
+		
+		void pop_back() {
+			if (!_size)
+				return ;
+			_size--;
+			_alloc.destroy(_buffer + _size);
+		}
+		
 		// reference operator[](size_type);
 		// const_reference operator[](size_type) const;
 		// reference at(size_type); 
@@ -208,43 +235,140 @@ namespace ft {
 		// iterator insert(const_iterator, iter, iter); //range
 		// iterator erase(const_iterator); //single elem
 		// iterator erase(const_iterator, const_iterator); //range
-		// void clear();
+		void clear() {
+			for (size_type i = 0; i < _size; i++) {
+					this->_alloc.destroy(this->_buffer + i);
+				}
+			_size = 0;
+		}
 		// template<class iter>
 		// void assign(iter, iter); //range 
-		// void assign(size_type, const T&); //fill
+		void assign(size_type n, const T& value) {
+			T* substitute;
+			
+			if (n > _capacity)
+			{
+				substitute = _alloc.allocate(n);
+				for (size_type i = 0; i < n; i++)
+				{
+					substitute[i] = value;
+					_alloc.destroy(_buffer + i);
+				}
+				_alloc.deallocate(_buffer, _capacity);
+				_capacity = n;
+				_buffer = substitute;
+				_size = n;
+			}
+			else if (n > _size)
+			{
+				for (size_type i = 0; i < n; i++)
+				{
+					if (i < _size)
+						_buffer[i] = value;
+					else
+						_alloc.construct(_buffer + i, value);
+				}
+				_size = n;
+			}
+			else
+			{
+				for (size_type i = 0; i < n; i++)
+					_buffer[i] = value;
+			}
+				
+		}
 
-		// void swap(vector&);
-		// size_type size() const;
-		// void resize (size_type, value_type val = value_type());
-		// size_type max_size() const;
-		// bool empty() const;
-		// size_type capacity() const;
-		// void reserve (size_type n);
+		void swap(vector &x) {
+			T 			*temp;
+			size_type 	temp_size;
+			size_type 	temp_capacity;
+			
+
+			temp = x._buffer;
+			temp_size = x._size;
+			temp_capacity = x._capacity;
+			x._buffer = _buffer;
+			x._size = _size;
+			x._capacity = _capacity;
+			_buffer = temp;
+			_size = temp_size;
+			_capacity = temp_capacity;
+		}
+		size_type size() const { return (_size); }
 		
-		// A get_allocator() const;
+		void resize (size_type n, value_type val = value_type()) {
+			if (n < _size)
+				for (size_type i = n; i < _size; i++)
+					_alloc.destroy(_buffer + i);
+			else 
+			{
+				if (n > _capacity)
+					this->reallocateNCopy(n);
+				for (size_type i = _size; i < n; i++)
+					_alloc.construct(_buffer + i, val);
+			}
+			_size = n;	
+		}
+		
+		size_type max_size() const {
+   			
+    		return (_alloc.max_size());
+		}
+
+		bool empty() const { return (!_size); }
+		size_type capacity() const { return (_capacity); }
+		
+		void reserve (size_type n) {
+			if (n > _capacity)
+				reallocateNCopy(n);
+		}
+		
+		A get_allocator() const { return (_alloc); }
 		private:
 			T *			_buffer;
 			A			_alloc;
-			size_type	_nbElem;
-			size_type	_space;
+			size_type	_size;
+			size_type	_capacity;
+
+			void		reallocateNCopy(size_type n) {
+				T* substitute;
+			
+				substitute = _alloc.allocate(n);
+				for (size_type i = 0; i < _size; i++)
+				{
+					substitute[i] = _buffer[i];
+					_alloc.destroy(_buffer + i);
+				}
+				_alloc.deallocate(_buffer, _capacity);
+				_capacity = n;
+				_buffer = substitute;
+			}
+			
+			void		reallocate(size_type n) {
+				T* substitute;
+			
+				substitute = _alloc.allocate(n);
+				for (size_type i = 0; i < _size; i++)
+					_alloc.destroy(_buffer + i);
+				_alloc.deallocate(_buffer, _capacity);
+				_capacity = n;
+				_buffer = substitute;
+			}
 	};
 	
-	// template <class T, class A = std::allocator<T> >
-	// void swap(X<T,A>&, X<T,A>&);
+	template <class T>
+	void swap(vector<T>& a, vector<T>& b) {
+		a.swap(b);
+	}
 	
 	// template <class T, class Alloc>
-	// bool operator==(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+	bool operator==(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 	// bool operator!=(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 	// bool operator<(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 	// bool operator>(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 	// bool operator<=(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 	// bool operator>=(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
-	// template <typename T, class A>
-	// std::ostream & operator<<(std::ostream & o, const vector<T,A>& rhs) {
-	// 	for (typename A::size_type i = 0; i < rhs.getNbElem(); i++)
-	// 		o << (rhs.getBuff() + i) << " ";
-	// 	return (o);
-	// }
+	
 	
 }
 
